c15c1a63a3e6451fdc0561e4a9c17fbc121d1c15
acompagno <acompag@gmail.com>
2015-01-23 16:54:29 -0500

    Initial Commit

---
+++ src/main/java/walker/blue/core/lib/rect/RectCoordinates.java
@@ -0,0 +1,49 @@
+package walker.blue.core.lib.rect;
+
+/**
+ * Class representing the coordinate system being used
+ */
+public class RectCoordinates {
+
+    /**
+     * Coordinate values
+     */
+    private int x;
+    private int y;
+    private int z;
+
+    /**
+     * Class constructor. Sets the x, y, and z coordinates
+     */
+    public RectCoordinates(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    /**
+     * Get the value of the x coordinate
+     * @return int
+     */
+    public int getXValue() {
+        return x;
+    }
+
+    /**
+     * Get the value of the y coordiniate
+     *
+     * @return int
+     */
+    public int getYValue() {
+        return y;
+    }
+
+    /**
+     * Get the value of the z coordiniate
+     *
+     * @return int
+     */
+    public int getZValue() {
+        return z;
+    }
+}
