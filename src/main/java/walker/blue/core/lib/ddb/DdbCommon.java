95253557f531383bd6642c3170d2385cf539e258
Josh Facchinello <joshfacchinello@gmail.com>
2015-03-12 15:32:02 -0400

    Added a common file to hold shared dynamodb variables.

---
+++ src/main/java/walker/blue/core/lib/ddb/DdbCommon.java
@@ -0,0 +1,20 @@
+package walker.blue.core.lib.ddb;
+
+
+public class DdbCommon {
+
+    /**
+     * Name for the Building ID attribute
+     */
+    public static final String BUILDING_ID = "BuildingID";
+    /**
+     * Name of the DynamoDB table
+     */
+    public static final String TABLE_NAME = "BlueWalker";
+
+    public static final String JSON_DOCUMENT = "json";
+
+    public static final String NODE_DISTANCE = "NodeDistance";
+
+    public static final String FLOOR_HEIGHT = "FloorHeight";
+}
